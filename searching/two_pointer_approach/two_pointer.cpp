3566302b4b07a167b0652ab8a249d7421af3fb74
Abhisheksingh12345 <atching123@gmail.com>
2023-07-16 01:22:42 +0530

    calling fun()

--- searching/two_pointer_approach/two_pointer.cpp
+++ searching/two_pointer_approach/two_pointer.cpp
@@ -3,5 +3,8 @@ void fun(int arr[], int sum, int size);
 int main() {
     int arr[]{2,5,8,12,30};
     int size = sizeof(arr)/sizeof(arr[0]);
+    int sum = 12;
+    //this fun is used in sorted and un sorted array
+    fun(arr, sum, size);
     return 0;
 }
