0167aa9b8907a48366fd49ee99b75f3346095c02
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 20:07:32 +0530

    size of array and calling rotated_array()

--- searching/rotated_array/rotated.cpp
+++ searching/rotated_array/rotated.cpp
@@ -3,4 +3,6 @@ int rotated_array(int arr[], int element, int size);
 int main() {
     int arr[]{100,200,300,10,20,30,35};
     int element  = 100
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << rotated_array(arr, element, size);
 }
