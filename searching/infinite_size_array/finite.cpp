d7cbf4e5de43374a7d99157e208ce0be7093be1c
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 12:32:19 +0530

    search in finite no of element

--- searching/infinite_size_array/finite.cpp
+++ searching/infinite_size_array/finite.cpp
@@ -2,7 +2,7 @@
 int infinite_size_arry(int arr[], int search_elemet);
 int binary_search(int arr[], int search_element, int lower_bound, int upper_bound);
 using namespace std;
-int mian() {
+int main() {
     // suppose you are give  finite no of array
     int arr[] {1,10,15,20,40,45,60,70,80,85,90,91,99,100,150,122,130};
     // with the search element as x = 64
@@ -10,6 +10,7 @@ int mian() {
     cout << infinite_size_arry(arr, x);
     return 0;
 }
+
 int infinite_size_arry(int arr[], int seach_element) {
     if(arr[0] == seach_element)
     return 0;
