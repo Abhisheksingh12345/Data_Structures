d4e6b2e808e428d03c268820016df1809f8494a7
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 22:32:44 +0530

    lenear search algo

--- searching/rotated_array/rotated.cpp
+++ searching/rotated_array/rotated.cpp
@@ -8,6 +8,13 @@ int main() {
     int size = sizeof(arr)/sizeof(arr[0]);
     cout << rotated_array(arr, element, size);
 }
+int search(int arr[], int x, int l, int h) {
+    for(int i = 0; i <= h; i++) {
+        if(arr[i] == x)
+            return i;
+    }
+    return -1;
+}
 int rotated_array(int arr[], int x, int n) {
     int mid = n/2;
     if(arr[mid] == x) return mid;
