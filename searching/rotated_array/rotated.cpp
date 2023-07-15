f8126836b3f356a49644692d3b1a99eb74dcb73c
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 20:08:57 +0530

    .

--- searching/rotated_array/rotated.cpp
+++ searching/rotated_array/rotated.cpp
@@ -3,7 +3,7 @@ int rotated_array(int arr[], int element, int size);
 using namespace std;
 int main() {
     int arr[]{100,200,300,10,20,30,35};
-    int element  = 100
+    int element  = 100;
     int size = sizeof(arr)/sizeof(arr[0]);
     cout << rotated_array(arr, element, size);
 }
