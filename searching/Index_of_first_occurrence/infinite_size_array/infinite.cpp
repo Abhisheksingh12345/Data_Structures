dc02af62d7ba75626c229709d7be527026b51e68
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 11:56:29 +0530

    funts

---
+++ searching/Index_of_first_occurrence/infinite_size_array/infinite.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int infinite_size_arry(int arr[], int search_elemet);
+int binary_search(int arr[], int search_element, int lower_bound, int upper_bound);
+using namespace std;
+int mian() {
+    return 0;
+}
+
