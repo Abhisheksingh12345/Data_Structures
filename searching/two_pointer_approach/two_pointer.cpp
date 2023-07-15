d12f626f9c67b2019177d07750a0f137fde51bed
Abhisheksingh12345 <atching123@gmail.com>
2023-07-16 01:38:25 +0530

    values

--- searching/two_pointer_approach/two_pointer.cpp
+++ searching/two_pointer_approach/two_pointer.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int arr[]{2,5,8,12,30};
     int size = sizeof(arr)/sizeof(arr[0]);
-    int sum = 12;
+    int sum = 17;
     //this fun is used in sorted and un sorted array
     fun(arr, sum, size);
     return 0;
@@ -12,11 +12,11 @@ int main() {
 void fun(int arr[], int sum, int size) {
     int arr2[500]{0};
     arr2[abs(sum-arr[0])]++;
-    for(int i = 1; i < size; i++) {
+    for(int i = 0; i < size; i++) {
         if(arr2[i] > 0) {
             cout << arr[i] << " + " << i << " = " << sum;
+            break;
         }
         arr2[abs(sum-arr[i])]++;
-
     }
 }
