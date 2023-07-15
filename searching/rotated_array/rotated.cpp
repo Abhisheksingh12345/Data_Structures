f876e48b2620989600ff4b8e0e76e8317ac84b36
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 22:30:10 +0530

    leniorly search the element

--- searching/rotated_array/rotated.cpp
+++ searching/rotated_array/rotated.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 int rotated_array(int arr[], int element, int size);
+int search(int arr[], int element, int lower_bound, int upper_bound);
 using namespace std;
 int main() {
     int arr[]{100,200,300,10,20,30,35};
