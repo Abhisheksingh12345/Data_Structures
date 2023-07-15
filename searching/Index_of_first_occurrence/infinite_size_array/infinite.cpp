834ef7c98e2a43c2fc8047ae89fbfe46f5dde90c
Abhisheksingh12345 <atching123@gmail.com>
2023-07-15 11:59:45 +0530

    arr and elemet

--- searching/Index_of_first_occurrence/infinite_size_array/infinite.cpp
+++ searching/Index_of_first_occurrence/infinite_size_array/infinite.cpp
@@ -3,6 +3,11 @@ int infinite_size_arry(int arr[], int search_elemet);
 int binary_search(int arr[], int search_element, int lower_bound, int upper_bound);
 using namespace std;
 int mian() {
+    // suppose you are give  finite no of array
+    int arr[] {1,10,15,20,40,45,60,70,80,85,90,91,99,100,150,122,130};
+    // with the search element as x = 64
+    int x = 64;
+    cout << infinite_size_arry(arr, x);
     return 0;
 }
 
