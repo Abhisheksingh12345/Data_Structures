d9fdc64de84bc18e31acf407b17519ec113ff30b
Abhisheksingh12345 <atching123@gmail.com>
2023-07-16 01:02:55 +0530

    cases with peak element

--- searching/peak_element_search/peak.cpp
+++ searching/peak_element_search/peak.cpp
@@ -11,5 +11,12 @@ int peak(int arr[], int n) {
     int high = n-1;
     while(low <= high) {
         int mid = (low+high)/2;
+        if((mid == 0 || arr[mid] >= arr[mid-1]) && (mid == n-1 || arr[mid] >= arr[mid+1]))
+            return arr[mid];
+        if(mid > 0 && arr[mid] <= arr[mid-1])
+            high = mid-1;
+        else    
+            low = mid+1;
     }
+    return -1;
 }
