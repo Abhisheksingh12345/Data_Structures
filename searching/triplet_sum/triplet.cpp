000794337af9c433e34aae7372a6f37744f29700
Abhisheksingh12345 <atching123@gmail.com>
2023-07-17 22:06:17 +0530

    triplet fun()

--- searching/triplet_sum/triplet.cpp
+++ searching/triplet_sum/triplet.cpp
@@ -8,3 +8,41 @@ int main() {
     cout << triplet(arr, sum, size) ;
     return 0;
 }
+bool triplet(int arr[], int sum, int n) {
+    int start = 0;
+    int end = n-1;
+    int mid = end/2;
+    if(n == 1 && n == sum) {
+        return false;
+    } 
+    if(n == 2 && arr[start] + arr[end] == sum) {
+        return false;
+    }
+    if(arr[start]+ arr[mid] + arr[end] == sum) {
+        cout << "found";
+        return true;
+    }
+    while(mid > start && mid < end) {
+        if(arr[start] + arr[end]  > sum)
+            end--;
+        else if(arr[start] + arr[end] <  sum){
+            if(arr[start] + arr[mid] + arr[end] == sum) {
+                cout << "found";
+                return true;
+            }
+            if(arr[start] + arr[mid] + arr[end] > sum) {
+                if(mid > start) 
+                    mid--;
+                else
+                    end--;
+            } else {
+                if(mid < end) {
+                    mid++;
+                } else  
+                    start++;
+            }
+        } 
+    }
+    return false;
+
+}
