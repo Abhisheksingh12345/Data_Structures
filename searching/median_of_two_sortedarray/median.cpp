a90beb5673ed5a9b596373867d0b3de2a35217cf
Abhisheksingh12345 <atching123@gmail.com>
2023-07-17 22:47:14 +0530

    updating 0 -> 1 in example array

--- searching/median_of_two_sortedarray/median.cpp
+++ searching/median_of_two_sortedarray/median.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 double mediun(int [], int [], int size_1, int size_2);
+void fill_brr(int brr[], int arr[], int n);
 using namespace std;
 int main() {
     int arr[] {10,20,30,40,50};
@@ -8,6 +9,11 @@ int main() {
     int size_2 = sizeof(arr2)/ sizeof(arr2[0]);
     cout << mediun(arr, arr, size_1, size_2);
 }
+void fill_brr(int brr[], int arr[], int n) {
+    for(int i =0; i < n; i++) {
+        brr[arr[i]]++;
+    }
+} 
 double mediun(int arr[], int arr2[], int s1, int s2) {
     int brr[500] {0};
     fill_brr(brr, arr, s1);
