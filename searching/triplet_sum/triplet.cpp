730e97ece392a68e4bc076b97ddedc12ba581f14
Abhisheksingh12345 <atching123@gmail.com>
2023-07-17 22:06:27 +0530

    array

--- searching/triplet_sum/triplet.cpp
+++ searching/triplet_sum/triplet.cpp
@@ -2,7 +2,7 @@
 bool triplet(int [], int , int);
 using namespace std;
 int main() {
-    int arr[] {2,4,8,9,,20,40,41};
+    int arr[] {2,4,8,9,20,40,41};
     int sum = 32;
     int size = sizeof(arr)/ sizeof(arr[0]);
     cout << triplet(arr, sum, size) ;
