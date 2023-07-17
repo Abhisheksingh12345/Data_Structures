7f7c0b50e4c15ee7c0aa6a0d8473b89cc67bcace
Abhisheksingh12345 <atching123@gmail.com>
2023-07-17 21:24:11 +0530

    only for sorted array

--- searching/two_pointer_approach/two_pointer.cpp
+++ searching/two_pointer_approach/two_pointer.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 void fun(int arr[], int sum, int size);
+void fun2(int arr[], int sum, int );
 using namespace std;
 int main() {
     int arr[]{2,5,8,12,30};
@@ -7,9 +8,26 @@ int main() {
     int sum = 17;
     //this fun is used in sorted and un sorted array assuming you have unsorted array
     fun(arr, sum, size);
+    //this fun is used in only sorted array
+    fun2(arr, sum, size);
     return 0;
 } 
-
+void fun2(int arr[], int sum, int size) {
+    int i = 0;
+    int inner_sum = 0;
+    int j = size;
+    while( i < j) {
+        inner_sum = arr[i] + arr[j];
+        if(inner_sum == sum) {
+            cout << arr[j] << " + " << arr[i] << " = " << inner_sum;
+            break;
+        }
+        else if(inner_sum > sum)
+            j--;
+        else  
+            i++;
+    }
+}
 void fun(int arr[], int sum, int size) {
     int arr2[500]{0};
     arr2[abs(sum-arr[0])]++;
