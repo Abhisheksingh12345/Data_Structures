330e1a36c44c3ee8d9b31925f9f3d23f1297ecf7
Abhisheksingh12345 <atching123@gmail.com>
2023-07-17 21:55:17 +0530

    arr, sum , size , and function call

--- searching/triplet_sum/triplet.cpp
+++ searching/triplet_sum/triplet.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 bool triplet(int [], int , int);
+using namespace std;
 int main() {
+    int arr[] {2,4,8,9,,20,40,41};
+    int sum = 32;
+    int size = sizeof(arr)/ sizeof(arr[0]);
+    cout << triplet(arr, sum, size) ;
     return 0;
 }
