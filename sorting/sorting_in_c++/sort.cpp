6ae374dec11ef352f79dea736340414fd95633db
Mokey-D-luffy88 <captaine0luffy@gmail.com>
2023-08-06 19:48:39 +0530

    sort()

--- sorting/sorting_in_c++/sort.cpp
+++ sorting/sorting_in_c++/sort.cpp
@@ -10,16 +10,21 @@ int main() {
      * using sort(arg1, arg2)
      * sort argument_1 take the reference of 1st variable 
      * and argument_2 take the reference beyoung the last element of array or collection.
-     * 
-     * 
+     *
      */
     
     std::sort(arr, arr+n);
     for (int i : arr) {
        std::cout << i << " ";
     }
+    std::cout << std::endl;
     std::vector<int> arr_2 = {2,4,68,45,63,6,7,2,5,45,7,88,5,6,2,4,8};
-    
+    std::sort(arr_2.begin(), arr_2.end());
+    for (int i : arr_2) {
+       std::cout << i << " ";
+    }
+
+
 
 
 }
