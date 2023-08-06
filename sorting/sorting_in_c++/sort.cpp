b5a0d27b00407ae0e8f29730c55ba5dc6473587f
Mokey-D-luffy88 <captaine0luffy@gmail.com>
2023-08-06 19:56:06 +0530

    struct and customer comparition

--- sorting/sorting_in_c++/sort.cpp
+++ sorting/sorting_in_c++/sort.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+struct pair
+{
+    /* data */
+    int x, y;
+};
+
+bool comparing (pair p1, pair p2) {
+    return p1.x > p2.x;
+}
+
+
 int main() {
     // first with array 
     int arr[] = {4,56,7,9,8,2,3,4,6,78,5};
