2dfa98187fe48b9c3eff29f6039be91d13fe4217
Mokey-D-luffy88 <captaine0luffy@gmail.com>
2023-08-06 19:40:45 +0530

    algorithm module and import sorting from it

--- sorting/sorting_in_c++/sort.cpp
+++ sorting/sorting_in_c++/sort.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-#include <conio.h>
 #include <vector>
-#include <string>
-
+#include <algorithm>
 int main() {
     // first with array 
     int arr[] = {4,56,7,9,8,2,3,4,6,78,5};
     int n = sizeof(arr)/sizeof(arr[0]);
 
+    /**
+     * using sort(arg1, arg2)
+     * sort argument_1 take the reference of 1st variable 
+     * and argument_2 take the reference beyoung the last element of array or collection.
+     * 
+     * 
+     */
     
+    std::sort(arr, arr+n);
+
 }
