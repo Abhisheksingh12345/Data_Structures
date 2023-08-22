05839a17252cec6c0e706916c9d6f4c28108697e
Abhishek <atching123@gmail.com>
2023-08-22 22:18:49 +0530

    swapping

--- sorting/bubble_sort/bubble.cpp
+++ sorting/bubble_sort/bubble.cpp
@@ -2,18 +2,27 @@
 #include <vector>
 using namespace std;
 
+void swap(int arr[], int num1, int num2) {
+    int temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = arr[temp];
+}
 int bubble_sort(int arr[], int size) {
+
+    for (int i = 0; i < size-1; i++) {
+        for (int j = 0; j < size-i-1; j++) {
+            if(arr[j] > arr[j+1])
+                swap(arr, arr[j+1],arr[j]);
+        }
+    }
    return 0; 
 }
 int main() {
-    int arr[]{};
+    int arr[]{5,1,4,8,9,0};
     int size = sizeof(arr)/ sizeof(arr[0]);
     bubble_sort(arr, size);
     for(int i : arr) {
         cout << i << ' ';
     }
     return 0;
-
-
-
 }
