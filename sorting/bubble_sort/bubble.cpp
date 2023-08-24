9b02be39241223c3baf4f6a404f67e955027c0b6
Abhishek <atching123@gmail.com>
2023-08-24 23:07:14 +0530

    bubble sorting

--- sorting/bubble_sort/bubble.cpp
+++ sorting/bubble_sort/bubble.cpp
@@ -5,15 +5,19 @@ using namespace std;
 void swap(int arr[], int num1, int num2) {
     int temp = arr[num1];
     arr[num1] = arr[num2];
-    arr[num2] = arr[temp];
+    arr[num2] = temp;
 }
 int bubble_sort(int arr[], int size) {
-
+    int temp = 0;
     for (int i = 0; i < size-1; i++) {
         for (int j = 0; j < size-i-1; j++) {
             if(arr[j] > arr[j+1])
-                swap(arr, arr[j+1],arr[j]);
+                swap(arr, arr[j],arr[j+1]);
         }
+        for(int k = 0; k < size; k++) {
+            cout << arr[k] << ' ';
+        }
+        cout<<endl;
     }
    return 0; 
 }
