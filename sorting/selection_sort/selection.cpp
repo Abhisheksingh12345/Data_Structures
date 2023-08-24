888f8f133a1097a12ed45e46cfa22f214c6a627b
Abhishek <atching123@gmail.com>
2023-08-24 23:51:06 +0530

    selection sort

--- sorting/selection_sort/selection.cpp
+++ sorting/selection_sort/selection.cpp
@@ -2,19 +2,41 @@
 #include <vector>
 using namespace std;
 
-void swap(int arr[], int num1, int num2) {
-    int temp = arr[num1];
-    arr[num1] = arr[num2];
-    arr[num2] = temp;
+void swap(int arr[], int num1_i, int num2_i)
+{
+    int temp = arr[num1_i];
+    arr[num1_i] = arr[num2_i];
+    arr[num2_i] = temp;
 }
-int selection_sort(int arr[], int size) {
-   return 0; 
+int selection_sort(int arr[], int size)
+{
+    bool response = false;
+    int min_i = 0;
+    for (int i = 0; i < size-1; i++)
+    {
+        min_i = i;
+        for (int j = i; j < size; j++)
+        {
+            if (arr[min_i] > arr[j])
+            {
+                min_i = j;
+                response = true;
+            }
+        }
+        if (response)
+            swap(arr, i, min_i);
+        else
+            break;
+    }
+    return 0;
 }
-int main() {
-    int arr[]{5,1,4,8,9,0};
-    int size = sizeof(arr)/ sizeof(arr[0]);
+int main()
+{
+    int arr[]{5, 1, 4, 8, 9, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
     selection_sort(arr, size);
-    for(int i : arr) {
+    for (int i : arr)
+    {
         cout << i << ' ';
     }
     return 0;
