19276a7491c9faf9a398e20753e077c97eb667d8
Abhishek <atching123@gmail.com>
2023-08-27 15:51:29 +0530

    merge_sort_with O(n) and aux(1)

--- sorting/merge_sort/merge_sort.cpp
+++ sorting/merge_sort/merge_sort.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
-int[] merge(int arr[], int arr2[], int size1, int size2);
+int* merge(int arr[], int arr2[], int size1, int size2);
+
+int* merge(int arr[], int brr[],  int size1, int size2) {
+    int new_arr[size1 + size2] {0};
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while(i < size1 || j < size2) {
+        if(arr[i] >= brr[j]) {
+            new_arr[k] = brr[j];
+            j++;
+        } else {
+            new_arr[k] = arr[i];
+            i++;
+        }
+        if (i == size1) i = --size1;
+        if (j == size2) j = --size2;
+        k++;
+    }
+    return new_arr;
+}
 
 void display(int new_arr[], int size) {
     for(int i = 0; i  < size; i++) {
