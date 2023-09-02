c1350c627cbf5909089ea3cd78da34d8a78a46f5
Abhishek <atching123@gmail.com>
2023-09-03 01:46:56 +0530

    lomuto algo

--- sorting/partition_a_give_array/lomuto_partition.cpp
+++ sorting/partition_a_give_array/lomuto_partition.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 void lomuto_partition(int arr[], int h, int l) {
-    
+    int temp = 0;
+    int j = l-1;
+    int pivot = arr[h];
+    for(int i = l; i < h ; i++) {
+        if(arr[i] < pivot) {
+            j++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    temp = arr[j+1];
+    arr[j+1] = arr[h];
+    arr[h] = temp;
+
+    for(int i = 0; i <= h; i++) {
+        cout << arr[i] << ' ';
+    }
+
+
 }
 int main() {
     int arr[] {3,8,6,12,10,7};
