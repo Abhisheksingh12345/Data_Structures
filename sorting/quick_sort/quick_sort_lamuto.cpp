55927d83af8dd2da5896f94acdbe73eb9234c2d6
Abhishek <atching123@gmail.com>
2023-09-05 22:47:37 +0530

    display funciton

--- sorting/quick_sort/quick_sort_lamuto.cpp
+++ sorting/quick_sort/quick_sort_lamuto.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 int lamuto_partition(int [], int , int);
 void qsort(int[], int, int);
+void display(int arr[], int high) {
+    for(int i = 0; i < high; i++)
+        cout << arr[i] << ' ';
+}
 int main() {
     int arr[] {8,4,7,9,3,10,5};
     int size = (sizeof(arr)/sizeof(arr[0]));
     int low = 0;
     int high = size-1;
     qsort(arr, low, high);
-    
+    display(arr, size);
+
 }
+
 void qsort(int arr[], int low, int high) {
     if(low < high) {
         int p = lamuto_partition(arr, low, high);
