a6fd0a8996e47a181bd8fee0b785cbb41af45a09
Abhishek <atching123@gmail.com>
2023-09-11 13:07:29 +0530

    soritng and comparing each element with its neighbur

--- sorting/mini_diff_in_arr/mini.cpp
+++ sorting/mini_diff_in_arr/mini.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int getMiniDiff(int arr[], int size);
-void main () {
-    int arr[] {1,8,12,5,18};
+int main () {
+    int arr[] {18};
+    cout << arr[-1] << endl;
     int size = sizeof(arr)/ sizeof(arr[0]);
     cout << "Mini Diff " << getMiniDiff(arr, size);
+    return 0;
 }
 int getMiniDiff(int arr[], int size) {
+    sort(arr, arr+size);
+    int res = INT32_MAX;
+    for(int i = size-2; i > -1; i--) {
+        res = min(res, (arr[i+1] - arr[i]));
+    }
+    return res;
 }
