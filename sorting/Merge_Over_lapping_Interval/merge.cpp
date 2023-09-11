76b63b20c2011c09c0bffcc9784a057b53828690
Abhishek <atching123@gmail.com>
2023-09-11 20:43:23 +0530

    pairs

--- sorting/Merge_Over_lapping_Interval/merge.cpp
+++ sorting/Merge_Over_lapping_Interval/merge.cpp
@@ -2,8 +2,8 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-vector<pair<int, int>> gt_pairs(int[][2], int);
-pair<int ,int> getpair(int first, int second);
+vector<pair<int, int>> gt_pairs(vector<pair<int, int>>, int);
+pair<int, int> getpair(int first, int second);
 
 struct couple
 {
@@ -11,37 +11,55 @@ struct couple
     int y;
 };
 
-bool cmp(couple x_1, couple x_2) {
+bool cmp(couple x_1, couple x_2)
+{
     return x_1.x < x_2.x;
 }
 
-int main() {
-    int arr[][2] {{1,3}, {2,4}, {5,7}, {6,8}};
-    int size = sizeof(arr)/ sizeof(arr[0][0]);
-    vector<pair<int, int>> new_arr = gt_pairs(arr, size);
+int main()
+{
+
+    // int arr[][2] {{1,3}, {2,4}, {5,7}, {6,8}};
+    // int size = sizeof(arr)/ sizeof(arr[0][0]);
+    vector<pair<int, int>> arr;
+    arr.push_back(getpair(2, 7));
+    arr.push_back(getpair(3, 15));
+    arr.push_back(getpair(5, 10));
+    arr.push_back(getpair(18, 30));
+
+    vector<pair<int, int>> new_arr = gt_pairs(arr, arr.size());
+    for (int i = 0; i < new_arr.size(); i++)
+    {
+        cout << new_arr[i].first << ' ' << new_arr[i].second << endl;
+    }
+
     return 0;
 }
 
-pair<int , int> getpair(int first, int second) {
+pair<int, int> getpair(int first, int second)
+{
     pair<int, int> p;
     p.first = first;
     p.second = second;
     return p;
 }
 
-vector<pair<int, int>> gt_pairs(int arr[][2], int size) {
-    sort(arr, arr+size, cmp);
+vector<pair<int, int>> gt_pairs(vector<pair<int, int>> arr, int size)
+{
+    // sort(arr.begin(), arr.end(), cmp);
     vector<pair<int, int>> utter;
-    int start = min(arr[0][0], arr[0][1]);
+    int start = min(arr[0].first, arr[0].second);
     int min_, max_ = 0;
-    for(int i = 0; i < size-1; i++) {
-            max_ = max(max_, max(arr[i][0],arr[i][1]));
-            min_ = min(arr[i+1][0],arr[i+1][1]);
-            if(max_ < min_) {
-                utter.push_back(getpair(start, max_));
-                start = min(arr[i+1][0], arr[i+1][1]);
-            }
+    for (int i = 0; i < size - 1; i++)
+    {
+        max_ = max(max_, max(arr[i].first, arr[i].second));
+        min_ = min(arr[i + 1].first, arr[i + 1].second);
+        if (max_ < min_)
+        {
+            utter.push_back(getpair(start, max_));
+            start = min(arr[i + 1].first, arr[i + 1].second);
+        }
     }
-    utter.push_back(getpair(start, max(arr[size-1][0], arr[size-1][1])));
+    utter.push_back(getpair(start, max(arr[size - 1].first, arr[size - 1].second)));
     return utter;
 }
