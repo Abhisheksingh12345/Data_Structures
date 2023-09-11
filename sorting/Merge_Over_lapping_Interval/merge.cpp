9d995e8ce8c6e10ed68205684e4d3b80a2013401
Abhishek <atching123@gmail.com>
2023-09-11 19:33:41 +0530

    array and size

--- sorting/Merge_Over_lapping_Interval/merge.cpp
+++ sorting/Merge_Over_lapping_Interval/merge.cpp
@@ -3,5 +3,7 @@
 using namespace std;
 vector<pair<int, int>> gt_pairs(int[], int size);
 int main() {
+    int arr[][2] {{1,3}, {2,4}, {5,7}, {6,8}};
+    int size = sizeof(arr)/ sizeof(arr[0][0]);
     return 0;
 }
