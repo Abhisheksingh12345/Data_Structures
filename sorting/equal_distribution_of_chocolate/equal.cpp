cba2d9f6f88d7fa4ea7f8c73428f5ee7b8f0f577
Abhishek <atching123@gmail.com>
2023-09-11 14:30:46 +0530

    function signature and calling

--- sorting/equal_distribution_of_chocolate/equal.cpp
+++ sorting/equal_distribution_of_chocolate/equal.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
-vector<int> getPackets(int [], int size);
+vector<int> getPackets(int arr[], int size, int m);
 using namespace std;
 void main() {
     int arr[] {7,3,2,4,9,12,15};
     int size = sizeof(arr)/ sizeof(arr[0]);
-    vector<int> packets = getPackets(arr, size);
+    int m = 3;
+    vector<int> packets = getPackets(arr, size, m);
     for(int i : packets) 
         cout << i << ' ';
 }
-vector<int> getPackets(int arr[], int size) {
+vector<int> getPackets(int arr[], int size, int m) {
     vector<int> packets;
 
 }
