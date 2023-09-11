dc19a4057ceb9b52f85593a983effb5899b550a5
Abhishek <atching123@gmail.com>
2023-09-11 19:40:04 +0530

    structure, fucntion calling

--- sorting/Merge_Over_lapping_Interval/merge.cpp
+++ sorting/Merge_Over_lapping_Interval/merge.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<pair<int, int>> gt_pairs(int[], int size);
+struct couple
+{
+    int x;
+    int y;
+};
+
+bool cmp(couple x_1, couple x_2) {
+    return x_1.x < x_2.x;
+}
+vector<pair<int, int>> gt_pairs(int[][2], int);
 int main() {
     int arr[][2] {{1,3}, {2,4}, {5,7}, {6,8}};
     int size = sizeof(arr)/ sizeof(arr[0][0]);
+    vector<pair<int, int>> new_arr = gt_pairs(arr, size);
     return 0;
+}
+vector<pair<int, int>> gt_pairs(int arr[][2], int size) {
+    sort(arr, arr+size, cmp);
+
 }
