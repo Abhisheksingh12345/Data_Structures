f1159d1f0b6bdbcd1d8ea80ccea94a52a4b80014
Abhishek <atching123@gmail.com>
2023-09-20 00:37:04 +0530

    maine() and dispay()

--- matrix/Transpose_of_matrix/transpose.cpp
+++ matrix/Transpose_of_matrix/transpose.cpp
@@ -3,8 +3,30 @@
 using namespace std;
 
 void tranpose(int * arr[], int row) {
+    
 
 }
+void traverse(int *arr[], int row)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+}
 int main() {
+    int* arr[4];
+    for(int i = 0; i < 4; i++) 
+        arr[i] = new int[4];
+    int a = 0;
+    for(int i = 0; i < 4 ;i++) 
+        for(int j = 0; j < 4; j++)  
+            arr[i][j] = ++a;
+    cout <<"Normal Traversal" << endl;
+    traverse(arr , 4);
+    cout << "Transpose Traversal" << endl;
+    tranpose(arr, 4);
+    
     return 0;
 }
