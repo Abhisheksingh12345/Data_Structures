1500ce07dabdf6e44c5e42d206d46cd9a253a768
Abhishek <atching123@gmail.com>
2023-09-20 00:06:02 +0530

    snake Traversal

--- matrix/snake_pattern/snaek.cpp
+++ matrix/snake_pattern/snaek.cpp
@@ -3,25 +3,52 @@
 const int _C = 4;
 using namespace std;
 
-void snake(int *arr[][_C], int row)
+void snake(int *arr[], int row)
 {
-}
-void traverse(int *arr[][_C], int row) {
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j<  _C; j++)
-            cout << arr[i][j] <<" ";
+    int j = 0;
+    for (int i = 0; i < row; i++)
+    {
+        if (j == 0)
+        {
+            while (j < _C)
+            {
+                cout << arr[i][j] << " ";
+                j++;
+            }
+            j--;
+        }
+        else
+        {
+            while (j > -1)
+            {
+                cout << arr[i][j] << " ";
+                j--;
+            }
+            j++;
+        }
         cout << endl;
     }
 }
-int maine()
+void traverse(int *arr[], int row)
 {
-    int *arr[][_C];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < _C; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main()
+{
+    //demo();
+    int *arr[4];
     for (int i = 0; i < 4; i++)
         arr[i] = new int[_C];
     int a = 0;
     for (int i = 0; i < 4; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < _C; j++)
         {
             arr[i][j] = ++a;
         }
@@ -29,4 +56,6 @@ int maine()
     cout << "Normal Traverse" << endl;
     traverse(arr, 4);
     cout << "Snake Traverse" << endl;
+    snake(arr, 4);
+    return 0;
 }
