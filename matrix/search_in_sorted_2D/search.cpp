861adab8d503f8878995b04ab834db4068f303d7
Abhishek <atching123@gmail.com>
2023-09-21 00:37:39 +0530

    vales

--- matrix/search_in_sorted_2D/search.cpp
+++ matrix/search_in_sorted_2D/search.cpp
@@ -11,23 +11,23 @@ void isfound(int arr[][C], int x) {
     bool found = false;
     int i = 0;
     for(; i < R; i++) {
-        while(low < high) {
-            mid = (low + high)/2;
+        while(low <= high) {
+            mid = (high+low)/2;
             if(arr[i][mid] == x){
-                found != found;
+                found = !found;
                 break;
             } else if(arr[i][mid] < x)
-                low = mid;
+                low = mid + 1;
             else
-                high = mid;
+                high = mid- 1;
         }
         if (found)
             break;
         low = 0;
-        high = 0;
+        high = C-1;
     }
     if(found)
-        cout<<"found at ("<< i <<', '<<mid <<')';
+        cout<<"found at ("<< i <<", "<<mid <<')';
     else
         cout <<"not found";
 
@@ -40,6 +40,27 @@ int main(){
         {27,29,37,48},
         {32,33,39,50}
     };
-    isfound(arr, 29);
+    int x = 29;
+    // int brr[]{27,29,37,48};
+    // int mid = 0;
+    // int low = 0;
+    // int high = C-1;
+    // bool found = false;
+    // int i = 0;
+    // while(low < high) {
+    //     mid = (low + high)/2;
+    //     if(brr[mid] == x){
+    //         found = !found;
+    //         break;
+    //     } else if(brr[mid] < x)
+    //             low = mid;
+    //         else
+    //             high = mid;
+    // }
+    // if(found)
+    //     cout<<"found at ("<< i <<", "<<mid <<')';
+    // else
+    //     cout <<"not found";
+     isfound(arr, x);
     return 0;
 }
