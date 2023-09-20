bc7f4628eb117ff1fb337d62e3eb9ae94987af8b
Abhishek <atching123@gmail.com>
2023-09-20 18:50:32 +0530

    variable initialisation using in binary search

--- matrix/search_in_sorted_2D/search.cpp
+++ matrix/search_in_sorted_2D/search.cpp
@@ -5,7 +5,16 @@ const int R = 4;
 const int C = 4;
 
 void isfound(int *arr[], int x) {
+    int mid = 0;
+    int low = 0;
+    int high = C-1;
+    for(int i = 0; i < R; i++) {
+        while(low < high) {
 
+        }
+        low = 0;
+        high = 0;
+    }
 
 
 }
