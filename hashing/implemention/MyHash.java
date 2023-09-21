4c5aa6d8223b815b64ba8e0a547dbbb3e81589be
Abhishek <atching123@gmail.com>
2023-09-22 02:08:34 +0530

    function functionalities

--- hashing/implemention/MyHash.java
+++ hashing/implemention/MyHash.java
@@ -7,22 +7,40 @@ public class MyHash {
     private ArrayList<LinkedList<Integer>> hash_table;
     MyHash(int bucket_size) {
         this.bucket_size = bucket_size;
-        hash_table = new ArrayList<LinkedList<Integer>>(bucket_size);
+        hash_table = new ArrayList<LinkedList<Integer>>();
+        for(int i = 0; i < bucket_size; i++)
+            hash_table.add(new LinkedList<Integer>());
 
     }
     public int table_size(){
         return hash_table.size();
     }
+    private int get_key(int value) {
+        return value % bucket_size;
+    }
     public boolean search(int value) {
+        LinkedList<Integer> list = hash_table.get(get_key(value));
+        for(Integer i : list){
+            if(i == value)
+                return true;
+        }
         return false;
     }
     public boolean insert(int value) {
-        return false;
+        LinkedList<Integer> list = hash_table.get(get_key(value));
+        list.add(value);
+        return true;
     }
     public boolean delete(int value) {
+        if(search(value)) {
+            LinkedList<Integer> list = hash_table.get(get_key(value));
+            list.remove(value);
+            return true;
+        }
         return false;
     }
     public static void main(String[] args) {
-    
+        MyHash myHash = new MyHash(7);
+        // System.out.println("My bucket size is : "+ myHash.hash_table[0].size());
     }
 }
