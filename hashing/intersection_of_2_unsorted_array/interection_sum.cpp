b469e0b40ee844ee40876a38f30357dd4c338dd2
Abhishek <atching123@gmail.com>
2023-09-23 17:52:56 +0530

    2 array with diff size and funciton call

--- hashing/intersection_of_2_unsorted_array/interection_sum.cpp
+++ hashing/intersection_of_2_unsorted_array/interection_sum.cpp
@@ -2,3 +2,10 @@
 #include <unordered_set>
 using namespace std;
 void intersect_elements(int [], int , int [], int);
+int main() {
+    int arr[]{10,15,20,25,30,50};
+    int a_size = sizeof(arr)/sizeof(arr[0]);
+    int brr[]{30,5,15,80};
+    int b_size = sizeof(brr)/sizeof(brr[0]);
+    intersect_elements(arr, a_size, brr, b_size);
+}
