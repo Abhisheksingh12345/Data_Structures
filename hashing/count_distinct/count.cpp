46d056fb17b133d6e68a17711d0da4d1d9eb2e18
Abhishek <atching123@gmail.com>
2023-09-23 15:58:34 +0530

    fun declarations

--- hashing/count_distinct/count.cpp
+++ hashing/count_distinct/count.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <unordered_set>
 using namespace std;
+int count_distinct(int arr[], int size) {
+    
+}
 int main(){
     int arr[] {15,12,13,12,13,13,18};
     int size = sizeof(arr)/sizeof(arr[0]);
