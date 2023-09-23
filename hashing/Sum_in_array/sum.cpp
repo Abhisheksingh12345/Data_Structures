8c6a121fc5207caab09a1ad2f64dd0a2f73b08e5
Abhishek <atching123@gmail.com>
2023-09-23 18:11:39 +0530

    calling

--- hashing/Sum_in_array/sum.cpp
+++ hashing/Sum_in_array/sum.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 bool isSumContaine(int [], int);
 int main() {
-    
+    int arr[] {3,2,8,15,-8};
+    int sum = 17;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << isSumContaine(arr, sum);
 }
