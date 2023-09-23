dd1386c53eaf7ddb485daf98b1973e47406d3545
Abhishek <atching123@gmail.com>
2023-09-23 16:05:04 +0530

    size and array

--- hashing/frequency_of_array_element/frequency.cpp
+++ hashing/frequency_of_array_element/frequency.cpp
@@ -2,3 +2,7 @@
 #include <unordered_map>
 using namespace std;
 void frequency_show(int [], int );
+int main() {
+    int arr[] {10,12,10,15,10,20,12,12};
+    int size = sizeof(arr)/sizeof(arr[0]);
+}
