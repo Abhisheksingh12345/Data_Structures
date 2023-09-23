fd158cd3814d91626b83ece213daddc3f6f23f03
Abhishek <atching123@gmail.com>
2023-09-23 18:03:12 +0530

    two array with n , m size

--- hashing/union_of_2_unsorted_array/union.cpp
+++ hashing/union_of_2_unsorted_array/union.cpp
@@ -2,5 +2,9 @@
 #include <unordered_set>
 using namespace std;
 int main() {
+    int arr[] {15,20,5,15};
+    int brr[] {15,15,15,20,10};
+    int a = sizeof(arr)/ sizeof(arr[0]);
+    int b = sizeof(brr) /sizeof(brr[0])''
     return 0;
 }
