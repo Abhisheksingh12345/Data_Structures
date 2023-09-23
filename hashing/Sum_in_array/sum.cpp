cfac9f4b27dd0528d01c8b0729ee5051e84b2042
Abhishek <atching123@gmail.com>
2023-09-23 18:19:02 +0530

    methos 1 O(n) but O(9999)

--- hashing/Sum_in_array/sum.cpp
+++ hashing/Sum_in_array/sum.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <unordered_set>
 using namespace std;
-bool isSumContaine(int [], int);
+bool isSumContaine(int [], int, int);
 int main() {
     int arr[] {3,2,8,15,-8};
     int sum = 17;
     int size = sizeof(arr)/sizeof(arr[0]);
-    cout << isSumContaine(arr, sum);
+    cout << isSumContaine(arr, sum, size);
+}
+bool isSumContaine(int arr[], int sum, int size) {
+    //Methos 1 :
+        int b[9999] {0};
+        for(int i = 0 ;i < size; i++) {
+            if(b[arr[i]] > 0)
+                return true;
+            b[(sum-arr[i])]++;
+        }
+        return false;
 }
