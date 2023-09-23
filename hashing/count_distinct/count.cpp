2d0ecec0f32a46cca3a49bbfd532aa5ed3b089d7
Abhishek <atching123@gmail.com>
2023-09-23 16:00:10 +0530

    use unordered set to store data

--- hashing/count_distinct/count.cpp
+++ hashing/count_distinct/count.cpp
@@ -2,7 +2,10 @@
 #include <unordered_set>
 using namespace std;
 int count_distinct(int arr[], int size) {
-    
+    unordered_set<int> s;
+    for(int i = 0; i < size; i++)
+        s.insert(arr[i]);
+    return s.size()
 }
 int main(){
     int arr[] {15,12,13,12,13,13,18};
