ae5caa56c4072cacf9dba420085ea5ad347019e1
Abhishek <atching123@gmail.com>
2023-09-23 16:00:28 +0530

    return size

--- hashing/count_distinct/count.cpp
+++ hashing/count_distinct/count.cpp
@@ -5,7 +5,7 @@ int count_distinct(int arr[], int size) {
     unordered_set<int> s;
     for(int i = 0; i < size; i++)
         s.insert(arr[i]);
-    return s.size()
+    return s.size();
 }
 int main(){
     int arr[] {15,12,13,12,13,13,18};
