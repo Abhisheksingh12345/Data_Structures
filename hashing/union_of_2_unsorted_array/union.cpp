5d171e75b122a26121aade3d2a277d85d0cb3435
Abhishek <atching123@gmail.com>
2023-09-23 18:05:32 +0530

    intialing both array with unorderedset

--- hashing/union_of_2_unsorted_array/union.cpp
+++ hashing/union_of_2_unsorted_array/union.cpp
@@ -5,6 +5,13 @@ int main() {
     int arr[] {15,20,5,15};
     int brr[] {15,15,15,20,10};
     int a = sizeof(arr)/ sizeof(arr[0]);
-    int b = sizeof(brr) /sizeof(brr[0])''
+    int b = sizeof(brr) /sizeof(brr[0]);
+    unordered_set<int> set;
+    for(auto i : arr)
+        set.insert(i);
+    for(auto o : brr)
+        set.insert(o);
+    cout << set.size();
+    
     return 0;
 }
