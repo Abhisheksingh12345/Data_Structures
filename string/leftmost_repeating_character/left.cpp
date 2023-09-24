1a7499ce32c374a06e29028ac50a39d35ef6296c
Abhishek <atching123@gmail.com>
2023-09-24 21:13:56 +0530

    string initilization

--- string/leftmost_repeating_character/left.cpp
+++ string/leftmost_repeating_character/left.cpp
@@ -3,3 +3,8 @@
 #include <string>
 using namespace std;
 int left_most_repeating_char_index(string str);
+int main() {
+    string str = "geeksgforgeeks";
+    cout << left_most_repeating_char_index(str);
+    return 0;
+}
