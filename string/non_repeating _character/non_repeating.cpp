ad7bde7d872cd5c7e540cff9d0520f7b5599c5b6
Abhishek <atching123@gmail.com>
2023-09-24 21:52:26 +0530

    main()

--- string/non_repeating _character/non_repeating.cpp
+++ string/non_repeating _character/non_repeating.cpp
@@ -3,5 +3,7 @@
 using namespace std;
 int non_repeating_first_char_index(string);
 int main() {
+    string str = "geeksforgeeks";
+    cout << non_repeating_first_char_index(str);
     return 0;
 }
