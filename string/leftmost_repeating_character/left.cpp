3b62e9903e863bd008fa08ddf845e9aa7d318a72
Abhishek <atching123@gmail.com>
2023-09-24 21:30:40 +0530

    method 1

--- string/leftmost_repeating_character/left.cpp
+++ string/leftmost_repeating_character/left.cpp
@@ -9,13 +9,15 @@ int main() {
     return 0;
 }
 int left_most_repeating_char_index(string str) {
-    int arr[256]{0};
-    for(auto i : str)
-        arr[(int)i]++;
-    for(int i = 0; i < str.size(); i++) 
-        if(arr[(int)str[i]] > 1)
-            return i;
-    return -1;
+    // Method 1
+    // int arr[256]{0};
+    // for(auto i : str)
+    //     arr[(int)i]++;
+    // for(int i = 0; i < str.size(); i++) 
+    //     if(arr[(int)str[i]] > 1)
+    //         return i;
+    // return -1;
+    
     
 
 }
