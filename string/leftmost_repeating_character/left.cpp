d7a338815cc1407987749e7230d2ff924b0faf8b
Abhishek <atching123@gmail.com>
2023-09-24 21:16:00 +0530

    intilizing array of 256 with ACsii value of vharacter

--- string/leftmost_repeating_character/left.cpp
+++ string/leftmost_repeating_character/left.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 int left_most_repeating_char_index(string str);
 int main() {
-    string str = "geeksgforgeeks";
+   string str = "geeksgforgeeks";
     cout << left_most_repeating_char_index(str);
     return 0;
+}
+int left_most_repeating_char_index(string str) {
+    int arr[256]{0};
+    for(auto i : str)
+        arr[(int)i]++;
 }
