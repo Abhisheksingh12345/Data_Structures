9910aa5f6898da6a2199802a968211e5d84611bd
Abhishek <atching123@gmail.com>
2023-09-28 00:36:12 +0530

    numJewels()

--- string/leetcode/problem_771/jewels.cpp
+++ string/leetcode/problem_771/jewels.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int numJewelsInStones(string jewels, string stones) {
+       int arr[256];
+       for(char c : jewels)
+        arr[c]++;
+        int count = 0;
+        int low = 0;
+        int high = stones.size()-1;
+        while(low < high) {
+            if(arr[stones[low]] > 0)
+                count++;
+            if(arr[stones[high]] > 0)
+                count++;
+            low++;
+            high--;
+        } 
+        return count;
+    }
 int main() {
+
     return 0
 }
