860279f5c6212e13c9a6b8e02d8a72f0a5bd45a5
Abhishek <atching123@gmail.com>
2023-10-02 01:31:06 +0530

    main()

--- linked_list/small_implementation/add_last.cpp
+++ linked_list/small_implementation/add_last.cpp
@@ -6,5 +6,14 @@ bool add_last(Node* head, int x){
     while(temp->addresss != NULL)
         temp = temp->addresss;
     temp->addresss = new Node(x);
-    
+}
+int main() {
+    Node* head = new Node(10);
+    Node* temp_1 = new Node(20);
+    Node* temp_2 = new Node(30);
+    head->addresss = temp_1;
+    temp_1->addresss = temp_2;
+    int x = 5;
+    head = add_last(head, x);
+    traversal(head);
 }
