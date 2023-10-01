0f93e33ecf8ae8cd6752e7f1bbbe3dfb1fd7a5f7
Abhishek <atching123@gmail.com>
2023-10-02 00:44:46 +0530

    structure node which define node structure

--- linked_list/small_implementation/traversing.cpp
+++ linked_list/small_implementation/traversing.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+struct Node
+{
+    int no;
+    Node* addresss;
+    Node(int no) {
+        this->no = no;
+        addresss = NULL;
+    }
+    /* data */
+};
