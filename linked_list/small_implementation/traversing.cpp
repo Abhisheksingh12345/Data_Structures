f97b4577bc178a156b140e2a39c19e59bcff2fca
Abhishek <atching123@gmail.com>
2023-10-02 00:51:33 +0530

    connecting node to each other

--- linked_list/small_implementation/traversing.cpp
+++ linked_list/small_implementation/traversing.cpp
@@ -27,6 +27,7 @@ int main() {
     Node* head = new Node(10);
     Node* temp_1 = new Node(20);
     Node* temp_2 = new Node(30);
-    
+    head->addresss = temp_1;
+    temp_1->addresss = temp_2;
 
 }
