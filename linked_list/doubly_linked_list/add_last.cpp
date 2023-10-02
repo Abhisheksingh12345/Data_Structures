9a371225c8403d5f9b942f77da8de94f8804ec53
Abhishek <atching123@gmail.com>
2023-10-02 19:44:19 +0530

    main()

--- linked_list/doubly_linked_list/add_last.cpp
+++ linked_list/doubly_linked_list/add_last.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 #include "node.h"
 using namespace std;
-
+int main() {
+    Node* head = new Node(10);
+    Node* temp1 = new Node(20);
+    Node* temp2 = new Node(30);
+    Node* temp3 = new Node(40);
+    Node* temp4 = new Node(50);
+    head->next = temp1;
+    temp1->pre = head;
+    temp1->next = temp2;
+    temp2->pre = temp1;
+    temp2->next = temp3;
+    temp3->pre = temp2;
+    temp3->next = temp4;
+    temp4->pre = temp3;
+    forward_traversal(head);
+    // backward_traversal(temp4);
+    Node* _head = add_first(head, 0);
+    cout << endl;
+    forward_traversal(_head);
+    return 0;
+}
