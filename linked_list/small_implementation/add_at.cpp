4d6e6601dac511c60a852f176bd5b88e1202d3b9
Abhishek <atching123@gmail.com>
2023-10-02 16:43:11 +0530

    trversal ()

--- linked_list/small_implementation/add_at.cpp
+++ linked_list/small_implementation/add_at.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include "node.h"
 using namespace std;
+void traversal(Node* head) {
+    if(head == NULL)
+    {
+        cout << "NULL";
+        return ;
+    }
+    Node* temp = head;
+    while(temp != NULL) {
+        cout<< temp->no << " ";
+        temp = temp->addresss;
+    }
+
+}
