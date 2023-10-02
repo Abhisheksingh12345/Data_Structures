7da3b2d938741128df763fcbd2ce8d60e8002e55
Abhishek <atching123@gmail.com>
2023-10-02 17:03:20 +0530

    main() with 4 node and connections

--- linked_list/small_implementation/del_at.cpp
+++ linked_list/small_implementation/del_at.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include "node.h"
 using namespace std;
+int main() {
+    Node* head = new Node(10);
+    Node* temp_1 = new Node(20);
+    Node* temp_2 = new Node(30);
+    head->addresss = temp_1;
+    temp_1->addresss = temp_2;
+    int x = 5;
+    int pos = 3;
+    add_at(head, x, pos);
+    traversal(head);
+}
