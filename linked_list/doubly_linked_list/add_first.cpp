ccbdeca80d2bf0619ba99d69a274d701f139c4fb
Abhishek <atching123@gmail.com>
2023-10-02 19:20:29 +0530

    connecting doubly linked link

--- linked_list/doubly_linked_list/add_first.cpp
+++ linked_list/doubly_linked_list/add_first.cpp
@@ -7,6 +7,14 @@ int main() {
     Node* temp2 = new Node(30);
     Node* temp3 = new Node(40);
     Node* temp4 = new Node(50);
-
+    head->next = temp1;
+    temp1->pre = head;
+    temp1->next = temp2;
+    temp2->pre = temp1;
+    temp2->next = temp3;
+    temp3->pre = temp2;
+    temp3->next = temp4;
+    temp4->pre = temp3;
+     
     return 0;
 }
