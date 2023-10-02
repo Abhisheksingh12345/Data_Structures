bfbfc86d17170f95a652b14167f4950063c3821a
Abhishek <atching123@gmail.com>
2023-10-02 17:03:59 +0530

    connection

--- linked_list/small_implementation/del_at.cpp
+++ linked_list/small_implementation/del_at.cpp
@@ -5,8 +5,10 @@ int main() {
     Node* head = new Node(10);
     Node* temp_1 = new Node(20);
     Node* temp_2 = new Node(30);
+    Node* temp_3 = new Node(40);
     head->addresss = temp_1;
     temp_1->addresss = temp_2;
+    temp_2->addresss = temp_3;
     int x = 5;
     int pos = 3;
     add_at(head, x, pos);
