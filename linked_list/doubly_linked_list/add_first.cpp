28150b5d1ad43c2271bc1d49909461d2ab7dabcd
Abhishek <atching123@gmail.com>
2023-10-02 19:16:36 +0530

    pointer repersenting nodes

--- linked_list/doubly_linked_list/add_first.cpp
+++ linked_list/doubly_linked_list/add_first.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     Node* head = new Node(10);
     Node* temp1 = new Node(20);
-    Node* temp1 = new Node(30);
-    Node* temp1 = new Node(40);
-    Node* temp1 = new Node(50);
+    Node* temp2 = new Node(30);
+    Node* temp3 = new Node(40);
+    Node* temp4 = new Node(50);
+
     return 0;
 }
