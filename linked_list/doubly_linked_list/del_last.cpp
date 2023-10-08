d9dca473036db217f918e0e8bf875e123ae84234
Abhishek <atching123@gmail.com>
2023-10-08 14:51:15 +0530

    int main() funcition with 4 nodes and connection

--- linked_list/doubly_linked_list/del_last.cpp
+++ linked_list/doubly_linked_list/del_last.cpp
@@ -1,1 +1,22 @@
 #incldue "node.h"
+
+int main() {
+    Node* head = new Node(10);
+    Node* temp1 = new Node(20);
+    Node* temp2 = new Node(30);
+    Node* temp3 = new Node(40);
+    Node* temp4 = new Node(50);
+    head->next = temp1;
+    temp1->pre = head;
+    temp1->next = temp2;
+    temp2->pre = temp1;
+    temp2->next = temp3;
+    temp3->pre = temp2;
+    temp3->next = temp4;
+    temp4->pre = temp3;
+    forward_traversal(head);
+    reverse(head);
+    cout << endl;
+    forward_traversal(head);
+    return 0;
+}
