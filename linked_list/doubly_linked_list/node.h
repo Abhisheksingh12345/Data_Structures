46b66f061a7febc71a3ba4253cd0b3d26ec58f32
Abhishek <atching123@gmail.com>
2023-10-08 14:21:00 +0530

    including basic funtion

--- linked_list/doubly_linked_list/node.h
+++ linked_list/doubly_linked_list/node.h
@@ -10,3 +10,23 @@ struct Node
     }
     /* data */
 };
+void backward_traversal(Node* tail) {
+    if(tail == NULL)
+        return ;
+    while(tail != NULL) {
+        cout << tail->data << " ";
+        tail = tail->pre;
+    }
+}
+void forward_traversal(Node* head) {
+    if(head == NULL)
+    {
+        cout << "NULL";
+        return ;
+    }
+    while(head != NULL) {
+        cout<< head->data << " ";
+        head = head->next;
+    }
+
+}
