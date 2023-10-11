c114f282bef2efa43bae2cc7cf342f0f50ed1a29
Abhishek <atching123@gmail.com>
2023-10-12 00:50:23 +0530

    node connected creation and node connections

--- linked_list/circular_linked_list/traversal.cpp
+++ linked_list/circular_linked_list/traversal.cpp
@@ -1,5 +1,15 @@
 #include "node.h"
 using namespace std;
 int main() {
+    Node* head = new Node(10);
+    Node* temp = new Node(20);
+    Node* temp1 = new Node(30);
+    Node* temp2 = new Node(40);
+    head->next = temp;
+    temp->next = temp1;
+    temp1->next = temp2;
+    temp2->next = head;
+    forward_traversal(head);
+
     return 0;
 }
