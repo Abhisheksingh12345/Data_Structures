2cfa84fa506c3d6f43a04f3d1c3e8c731c0b5fdc
Abhishek <atching123@gmail.com>
2024-06-19 00:10:59 +0530

    algo

--- string/non_repeating _character/non_repeating.cpp
+++ string/non_repeating _character/non_repeating.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <limits.h>
 using namespace std;
 int non_repeating_first_char_index(string);
 int main() {
@@ -8,13 +9,17 @@ int main() {
     return 0;
 }
 int non_repeating_first_char_index(string str) {
-    int non_repeating = -1;
-    bool arr[256]{};
-    for(int i = str.size() -1; i > -1; i--) {
-        if(!arr[str[i]])
-            non_repeating = i;
-        else   
-            arr[str[i]] = true;
+        int arr[256]{-1};
+        fill(arr,arr+ 256, -1);
+        for(int i = 0; i < str.size(); i++) {
+            if(arr[str[i]] == -1)
+                arr[str[i]] = i;
+            else
+                arr[str[i]] = -2;
+        }
+        int res = INT_MAX;
+        for(int i = 0; i < 256; i++)
+            if(arr[i] > -1)
+                res = min(res, arr[i]);
+        return (res == INT_MAX) ? -1 : res;
     }
-    return non_repeating;
-}
